aafcfb41947ba63ef9167c3ad46fa0cf814b0a0b
RavinduTharaka1998 <68723931+RavinduTharaka1998@users.noreply.github.com>
2023-03-26 21:00:55 +0530

    Customer Part completed.

--- TrainFood/src/App.js
+++ TrainFood/src/App.js
@@ -1,19 +1,18 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Col, Container, Form, FormControl, Image, Nav, Navbar, Row} from "react-bootstrap";
 import {BrowserRouter as Router, Switch, Route, Link, BrowserRouter} from 'react-router-dom';
 
-import logo from './logo.jpg';
+
 import Create from './components/create.component';
-import Edit from './components/edit.component';
+import Edit from './components/Orderedit.component';
 import Index from './components/index.component';
 import AboutUs from './components/aboutus.component';
 import SignIn from './components/signin.component';
 import Landing from './components/LandingPage.component';
-import BuyerDash from './components/buyerdash.component';
-import SellerDash from './components/sellerdash.component';
-
-
+import CusProfile from './components/customerProfile.component';
+import AddOrder from './components/addOrder.component';
+import MyOrder from './components/myorder';
+import Payment from './components/Payment.component';
 
 
 class App extends Component{
@@ -25,13 +24,15 @@ class App extends Component{
                     <Switch>
                         <Route exact path='/' component={Landing}/>
                         <Route  path='/create' component={Create}/>
-                        <Route  path='/edit/:id' component={Edit}/>
+                        <Route  path='/editOrder/:id' component={Edit}/>
                         <Route  path='/index/:id' component={Index}/>
                         <Route  path='/aboutUs' component={AboutUs}/>
                         <Route  path='/signIn' component={SignIn}/>
-                        <Route  path='/BuyerDash' component={BuyerDash}/>
-                        <Route  path='/SellerDash' component={SellerDash}/>
                         <Route  path='/logout' component={Landing}/>
+                        <Route  path='/cusprofile/:id' component={CusProfile}/>
+                        <Route  path='/addorder/:id' component={AddOrder}/>
+                        <Route  path='/myorder/:id' component={MyOrder}/>
+                        <Route  path='/payment/:id' component={Payment}/>
                     </Switch>
                 </Router>
 
